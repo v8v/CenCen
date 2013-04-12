4f9b45bd0f8c523fa40aa0fac81150ff0292ef77
v8v <v8v@msn.com>
2013-04-12 21:35:47 +0800

    Update web.config.php

--- web.config.php
+++ web.config.php
@@ -1,23 +1,45 @@
-﻿<?php
- /*数据库连接*/
-@header("Content-type:text/html; charset=utf-8");
-$DB_HOST="127.0.0.1";  //数据库连接地址
-$DB_USER="root";       //数据库用户名称
-$DB_PASSWORD=""; //数据库密码
-$DB_DATABASE="jiuchenszchina";//数据库
-
-
- /*FTP*/
-$FTP_HOST="106.187.96.73";  //FTP地址
-$FTP_USER="jiuchenszchina";       //FTP用户名
-$FTP_PASSWORD="jiuchenszchina14"; //FTP密码
-
-/*网站URL*/
-$COMPANY_URL = "http://localhost";
-
-/*网站每页显示的记录数（分页）*/
-$PAGE_COUNT=12;
-
-/*网站语言版本*/
-$LANGUAGE="cn";
+﻿<?php
+
+ /*数据库连接*/
+
+@header("Content-type:text/html; charset=utf-8");
+
+$DB_HOST="127.0.0.1";  //数据库连接地址
+
+$DB_USER="root";       //数据库用户名称
+
+$DB_PASSWORD=""; //数据库密码
+
+$DB_DATABASE="jiuchenszchina";//数据库
+
+
+
+
+
+ /*FTP*/
+
+$FTP_HOST="106.187.96.73";  //FTP地址
+
+$FTP_USER="jiuchenszchina";       //FTP用户名
+
+$FTP_PASSWORD="jiuchenszchina1"; //FTP密码
+
+
+
+/*网站URL*/
+
+$COMPANY_URL = "http://localhost";
+
+
+
+/*网站每页显示的记录数（分页）*/
+
+$PAGE_COUNT=12;
+
+
+
+/*网站语言版本*/
+
+$LANGUAGE="cn";
+
 ?>
